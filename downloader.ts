f1a273c1f3ff3ac50300caa9d6c5231c4c907737
ShellTear <veetipunnonen02@gmail.com>
2020-11-03 04:29:55 +0200

    add pdf conversion

--- downloader.ts
+++ downloader.ts
@@ -1,86 +1,107 @@
-import axios from 'axios'
-import * as readline from 'readline'
-const cliProgress = require('cli-progress');
-var colors = require('colors');
+import axios from "axios";
+import * as readline from "readline";
+var colors = require("colors");
 
-const Downloader = require('nodejs-file-downloader')
-const cheerio = require('cheerio')
+const Downloader = require("nodejs-file-downloader");
+const cheerio = require("cheerio");
+const cliProgress = require("cli-progress");
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
+const pdf = new PDFDocument();
 
-const axiosins = axios.create()
-var pubcode = ''
-var array = []
+const axiosins = axios.create();
+var pubcode: string;
+var images: string[] = [];
+var array: string[] = [];
 
-const bar = new cliProgress.SingleBar(({
-  format: colors.blue('Downloading: [{bar}] {percentage}% | {value}/{total} pages | ETA: {eta}s'),
-  hideCursor: true
-}))
+const bar = new cliProgress.SingleBar({
+  format: colors.blue(
+    "Downloading: [{bar}] {percentage}% | {value}/{total} pages | ETA: {eta}s"
+  ),
+  hideCursor: true,
+});
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+});
 
 // main scraper and function
 const first = new Promise((resolve) => {
-  rl.question(colors.blue('Give me the code: '), (code) => {
-    const url = 'https://nhentai.net/g/' + code
-    pubcode = code
-    axiosins.get(url)
-      .then(
-        async response => {
-          var $ = cheerio.load(response.data)
+  rl.question(colors.blue("Give me the code: "), (code) => {
+    const url = "https://nhentai.net/g/" + code;
+    pubcode = code;
+    axiosins
+      .get(url)
+      .then(async (response) => {
+        var $ = cheerio.load(response.data);
 
-          // get pages
-          $('.lazyload').each(async (_index: any, value: any) => {
-            var link = $(value).attr('data-src')
+        // get pages
+        $(".lazyload").each(async (_index: any, value: any) => {
+          var link = $(value).attr("data-src");
 
-            // check what its going to download
-            var linktoadd: any
-            linktoadd = getLinks(link)
-            if (linktoadd != undefined) array.push(linktoadd)
-          })
-          rl.close()
-          await download()
-          resolve()
-        }).catch(console.error)
-  })
-})
+          // check what its going to download
+          var linktoadd: any;
+          linktoadd = getLinks(link);
+          if (linktoadd != undefined) array.push(linktoadd);
+        });
+        rl.close();
+        const images: string[] = await download();
+        resolve(images);
+      })
+      .catch(console.error);
+  });
+});
 
 const getLinks = (link: string) => {
   switch (true) {
-    case (!link.includes('thumb') && !link.includes('cover')):
+    case !link.includes("thumb") && !link.includes("cover"):
       // needed to get full resolution, 1st is the cover obviously
-      var splitted = link.split('.', -1)
-      splitted[0] = splitted[0].replace('https://t', 'https://i')
-      splitted[2] = splitted[2].substring(0, splitted[2].length - 1)
-      var last = splitted.join('.')
-      return last
+      var splitted = link.split(".", -1);
+      splitted[0] = splitted[0].replace("https://t", "https://i");
+      splitted[2] = splitted[2].substring(0, splitted[2].length - 1);
+      var last = splitted.join(".");
+      return last;
     default:
-      // we don't need the suggestions etc, hence we do nothing when those are encountered
+    // we don't need the suggestions etc, hence we do nothing when those are encountered
   }
-}
+};
 
 const download = async () => {
   bar.start(array.length, 0);
-    var i;
+  var i: number;
+
   for (i = 0; i < array.length; i++) {
     try {
       const downloader = new Downloader({
         url: array[i],
-        directory: './' + pubcode
-      })
-      await downloader.download()
-      bar.update(i + 1)
+        directory: "./" + pubcode,
+      });
+      await downloader.download();
+      images.push(`./${pubcode}/${i + 1}.jpg`);
+      bar.update(i + 1);
     } catch (error) {
-      console.log('Error downloading -> ' + error)
-      }
+      console.log("Error downloading -> " + error);
     }
-    bar.stop()
-  return
-}
+  }
+  bar.stop();
+  return images;
+};
+
+const makepdf = () => {
+  console.log(colors.blue("Creating pdf..."));
+  pdf.pipe(fs.createWriteStream(`./${pubcode}/${pubcode}.pdf`));
+
+  for (var i = 0; images.length > i; i++) {
+    var img = pdf.openImage(images[i]);
+    pdf.addPage({ size: [img.width, img.height] });
+    pdf.image(img, 0, 0);
+  }
+  pdf.end();
+};
 
 const flow = async () => {
-  await Promise.all([first])
-}
+  await Promise.all([first]).then(() => makepdf());
+};
 
-flow()
+flow();
